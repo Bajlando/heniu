2a12d92f080651e4d2111ed40ecac658e6864bc2
Bajlando <45641434+Bajlando@users.noreply.github.com>
2019-04-29 17:08:35 +0200

    Update bot.js

--- LICENSE.md/bot.js
+++ LICENSE.md/bot.js
@@ -4,16 +4,6 @@ const prefix = "$";
 const fs = require("fs");
 const Canvas = require('canvas');
 const snekfetch = require('snekfetch');
-  let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"))
-const serverStats = {
-    guildID: '303657307231879169',
-    totalUsersID: '499164441506480129',
-    memberCountID: '499164461459046410',
-    botCount: '499164691730268170',
-    clock: '500397109506211841',
-    time: '501863189458452480',
-    newUser: '499164704313442314'
-}
 
 client.on('ready', () => {
     console.log('I am ready!');
